3b964609467678791c865589699577ddd1706673
Mwita Chacha <jonahhchacha@gmail.com>
2022-03-05 04:34:35 +0300

    Consuming COVID api

--- core/views.py
+++ core/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+import requests
+from decouple import config
 
 def index(request):
+    url = "https://covid-19-statistics.p.rapidapi.com/reports/total"
+    
+    headers = {
+        'x-rapidapi-host': "covid-19-statistics.p.rapidapi.com",
+        'x-rapidapi-key': config("rapidapikey")
+        }
+    
+    response = requests.request("GET", url, headers=headers).json()
+    data = response['data']
+    update = data['last_update']
+    confirmed = data['confirmed']
+    recovered = data['confirmed_diff']
+    deaths = data['deaths']
+    active = data['active']
+    fatality = data['fatality']
 
-    return render(request, 'index.html')
+    context = {
+        'update':update, 'confirmed':confirmed, 'recovered':recovered, 'deaths':deaths, 'active':active, 'fatality':fatality
+    }
+    print(data)
+    return render(request, 'index.html', context)
